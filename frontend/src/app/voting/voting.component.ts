9192222d2456dd715a1a830ec807bf7a8cfb7c5d
Marco Germanier <marco@mrgt.me>
2022-10-16 22:39:18 +0100

    fixed walletConnect interaction

--- frontend/src/app/voting/voting.component.ts
+++ frontend/src/app/voting/voting.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit, Injectable, InjectionToken, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ethers } from 'ethers';
 
 @Component({
   selector: 'app-voting',
   templateUrl: './voting.component.html',
-  styleUrls: ['./voting.component.scss']
+  styleUrls: ['./voting.component.scss'],
 })
 export class VotingComponent implements OnInit {
-  
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  vote(proposal: number) {
+    console.log(proposal);
+  }
 }
--- frontend/src/app/wallet-connect/wallet-connect.component.ts
+++ frontend/src/app/wallet-connect/wallet-connect.component.ts
@@ -1,37 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { WalletService } from '../services/wallet.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-wallet-connect',
   templateUrl: './wallet-connect.component.html',
-  styleUrls: ['./wallet-connect.component.scss']
+  styleUrls: ['./wallet-connect.component.scss'],
 })
 export class WalletConnectComponent implements OnInit {
-
   public walletConnected: boolean = false;
-  public walletId: string = "";
+  public walletId: string = '';
 
-  constructor(private walletService: WalletService) { }
+  constructor(private walletService: WalletService, private router: Router) {}
 
   connectToWallet = async () => {
-    this.walletService.connectWallet();
-    window.location.reload();
-  }
+    this.walletService.connectWallet().then(() => this.checkWalletConnected());
+    // window.location.reload();
+  };
 
   checkWalletConnected = async () => {
     const accounts = await this.walletService.checkWalletConnected();
-    if(accounts.length > 0) {
+    if (accounts.length > 0) {
       this.walletConnected = true;
       this.walletId = accounts[0];
+      this.router.navigate(['/voting']);
     } else {
       this.walletConnected = false;
-      this.walletId = "";
+      this.walletId = '';
     }
-  }
+  };
 
   ngOnInit(): void {
     // this.connectToWallet();
-    this.checkWalletConnected();
   }
-
 }
