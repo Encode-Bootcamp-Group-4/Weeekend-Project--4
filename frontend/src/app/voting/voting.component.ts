baffde15adc3201d90d6cecda290173a9e14193a
Marco Germanier <marco@mrgt.me>
2022-10-18 00:18:13 +0100

    extract tx history via etherscan provider

--- frontend/src/app/voting/voting.component.ts
+++ frontend/src/app/voting/voting.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { WalletService } from '../services/wallet.service';
-import { ethers } from 'ethers';
+import { Contract, ethers } from 'ethers';
 import web3 from 'web3';
-import { MYTOKEN_ABI } from "../../assets/MyToken";
-import { TOKENIZEDBALLOT_ABI } from "../../assets/TokenizedBallot";
-import { WalletConnectComponent } from '../wallet-connect/wallet-connect.component';
+import { MYTOKEN_ABI } from '../../assets/MyToken';
+import { TOKENIZEDBALLOT_ABI } from '../../assets/TokenizedBallot';
+// import { WalletConnectComponent } from '../wallet-connect/wallet-connect.component';
 
 const MYTOKEN_ADDRESS = '0x4F4d6E7DD2Ad4F94338E2FcDE3d09c76F9F83945';
 const TOKENIZEDBALLOT_ADDRESS = '0x151427884B81dF42f8C8832A21E7Aa2D1CcF5fd3';
@@ -17,14 +17,13 @@ const tokenizedBallotAbi = TOKENIZEDBALLOT_ABI;
   templateUrl: './voting.component.html',
   styleUrls: ['./voting.component.scss'],
 })
-
 export class VotingComponent implements OnInit {
   walletId: string = '';
   provider: any;
   signer: any;
   myTokenAbi: any;
   MyTokenContract: any;
-  TokenizedBallotContract: any; 
+  TokenizedBallotContract: any;
   public ethereum;
 
   constructor(private walletService: WalletService) {
@@ -33,38 +32,53 @@ export class VotingComponent implements OnInit {
 
   ngOnInit(): void {
     this.walletService
-    .checkWalletConnected()
-    .then((accounts) => (this.walletId = accounts[0]));
+      .checkWalletConnected()
+      .then((accounts) => (this.walletId = accounts[0]));
   }
 
-  async requestTokens () {
+  async requestTokens() {
     throw new Error('Not implemented');
   }
 
   _delegate = new FormControl('');
 
-  async delegate (delegateAddress: any) {
+  async delegate(delegateAddress: any) {
     this.provider = new ethers.providers.Web3Provider(this.ethereum);
     this.signer = this.provider.getSigner();
-    const MyTokenContract = new ethers.Contract(MYTOKEN_ADDRESS, myTokenAbi.abi, this.signer);
+    const MyTokenContract = new ethers.Contract(
+      MYTOKEN_ADDRESS,
+      myTokenAbi.abi,
+      this.signer
+    );
     const delegateTx = await MyTokenContract['delegate'](delegateAddress);
     await delegateTx.wait();
-    console.log("Delegated!");
-  };
+    console.log('Delegated!');
+  }
 
   async vote(proposal: number) {
     this.provider = new ethers.providers.Web3Provider(this.ethereum);
     this.signer = this.provider.getSigner();
-    const TokenizedBallotContract = new ethers.Contract(TOKENIZEDBALLOT_ADDRESS, tokenizedBallotAbi.abi, this.signer);
-    const voteTx = await TokenizedBallotContract['vote'](proposal, ethers.utils.parseEther("1"));
+    const TokenizedBallotContract = new ethers.Contract(
+      TOKENIZEDBALLOT_ADDRESS,
+      tokenizedBallotAbi.abi,
+      this.signer
+    );
+    const voteTx = await TokenizedBallotContract['vote'](
+      proposal,
+      ethers.utils.parseEther('1')
+    );
     await voteTx.wait();
-    console.log("Voted!");
+    console.log('Voted!');
   }
 
   async getWinningProposal() {
     this.provider = new ethers.providers.Web3Provider(this.ethereum);
     this.signer = this.provider.getSigner();
-    const TokenizedBallotContract = new ethers.Contract(TOKENIZEDBALLOT_ADDRESS, tokenizedBallotAbi.abi, this.signer);
+    const TokenizedBallotContract = new ethers.Contract(
+      TOKENIZEDBALLOT_ADDRESS,
+      tokenizedBallotAbi.abi,
+      this.signer
+    );
     const winningProposal = await TokenizedBallotContract['winningProposal']();
     console.log(winningProposal);
     window.alert(`The winning proposal is ${winningProposal}`);
@@ -73,10 +87,33 @@ export class VotingComponent implements OnInit {
 
   async getWinningProposalName() {
     this.provider = new ethers.providers.Web3Provider(this.ethereum);
-    this.signer = this.provider.getSigner();
-    const TokenizedBallotContract = new ethers.Contract(TOKENIZEDBALLOT_ADDRESS, tokenizedBallotAbi.abi, this.signer);
+    const TokenizedBallotContract = new ethers.Contract(
+      TOKENIZEDBALLOT_ADDRESS,
+      tokenizedBallotAbi.abi,
+      this.signer
+    );
     const winningProposalName = await TokenizedBallotContract['winnerName']();
     console.log(winningProposalName);
-    window.alert(`Winner name: ${web3.utils.hexToUtf8(winningProposalName)}`)
+    window.alert(`Winner name: ${web3.utils.hexToUtf8(winningProposalName)}`);
+  }
+
+  async getVotingHistory() {
+    let etherscanProvider = new ethers.providers.EtherscanProvider(
+      5,
+      'E416Z7BKQ1GSW2J3Q9K6YHW2DYPHGZ97N8'
+    );
+    const TokenizedBallotContract = new ethers.Contract(
+      TOKENIZEDBALLOT_ADDRESS,
+      tokenizedBallotAbi.abi,
+      this.signer
+    );
+    etherscanProvider.getHistory(TOKENIZEDBALLOT_ADDRESS).then((history) => {
+      history.forEach((tx) => {
+        if (tx.data.length <= 138) {
+          const pepe = [tx.from, tx.hash, tx.data[73]];
+          console.log(pepe);
+        }
+      });
+    });
   }
 }
